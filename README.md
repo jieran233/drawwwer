083ce26b682c436edbbe57a4c44bc5f5dfddb9b5
jieran233 <jieranjiang233@gmail.com>
2023-08-27 00:51:11 +0800

    🤣👉some unnecessary update

--- README.md
+++ README.md
@@ -8,9 +8,24 @@
 - Install [stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) and install [sd-webui-controlnet](https://github.com/Mikubill/sd-webui-controlnet) plugin
 - Open SD WebUI and jump to ControlNet
   ![](https://s2.loli.net/2023/08/26/ju2QpcPCobIgtlL.png)
-- `python drawer.py <image_path>`
+- Run `python drawer.py <image_path>`
+  - Confirm processed image in the popup window, close it to continue
+  - After 5 seconds, the program will start drawing 
+
+## Feature
+
+- `scale`
+- `draw_position_point` (new!)
+
+  Draw the four corners of the picture to prevent misoperation caused by high-frequency automatic clicks
+
+  I believe you wouldn't want it to frantically click on your taskbar applications and then stage such a scene:
+
+  <img src="https://s2.loli.net/2023/08/27/qsk2YL19ZmuSbrP.jpg" alt="🤣👉" width="30%">
+- `interlaced` (new!)
+
+  🤣👉The actual test results show that there is almost no difference between interlaced scanning and progressive scanning in terms of time and GPU utilization.
 
 ## Roadmap (never)
 
 - Add PySide6 GUI
-- Add a feature that can mark the positioning points before starting to draw
--- drawer.py
+++ drawer.py
@@ -2,22 +2,58 @@ import cv2
 from img_process import img_process
 from img2lists import img2lists
 import mouse
-from time import sleep
+from time import time, sleep
 from sys import argv
 
 
-def draw(pixel_lists, scale=1):
+def draw(pixel_lists, scale=1, draw_position_point=True, interlaced=True):
     initial_pos = mouse.get_position()
-    current_pos = initial_pos
-    for row in pixel_lists:
-        current_pos = (initial_pos[0], current_pos[1] + 1*scale)
-        mouse.move(current_pos[0], current_pos[1])
-        for pixel in row:
-            current_pos = (current_pos[0] + 1*scale, current_pos[1])
+
+    def _draw_position_point():
+        x, y = len(pixel_lists[0])*scale, len(pixel_lists)*scale
+        points = [initial_pos, (initial_pos[0]+x, initial_pos[1]),
+                  (initial_pos[0], initial_pos[1]+y), (initial_pos[0]+x, initial_pos[1]+y)]
+        for point in points:
+            mouse.move(point[0], point[1])
+            mouse.click()
+        mouse.move(initial_pos[0], initial_pos[1])
+
+    if draw_position_point:
+        print(':: Drawing position point')
+        _draw_position_point()
+        print(':: Start drawing officially after 3 seconds')
+        sleep(3)
+
+    def _draw(_pixel_lists):
+        current_pos = initial_pos
+        for row in _pixel_lists:
+            current_pos = (initial_pos[0], current_pos[1] + 1 * scale)
+            if row == -1:
+                continue
             mouse.move(current_pos[0], current_pos[1])
-            if pixel == 1:
-                mouse.click()
-            print(f"[{'x' if pixel == 1 else ' '}]{current_pos}")
+            for pixel in row:
+                current_pos = (current_pos[0] + 1 * scale, current_pos[1])
+                mouse.move(current_pos[0], current_pos[1])
+                if pixel == 1:
+                    mouse.click()
+                print(f"[{'x' if pixel == 1 else ' '}]{current_pos}")
+
+    if interlaced:
+        def _interlaced_blank(original_list, even_or_odd):  # 0: even, 1: odd
+            modified_list = []
+            for i, row in enumerate(original_list):
+                if i % 2 == even_or_odd:
+                    modified_list.append(-1)
+                else:
+                    modified_list.append(row)
+            return modified_list
+
+        even_row = _interlaced_blank(pixel_lists, 0)
+        _draw(even_row)
+        odd_row = _interlaced_blank(pixel_lists, 1)
+        _draw(odd_row)
+    else:
+        _draw(pixel_lists)
 
 
 if __name__ == '__main__':
@@ -27,6 +63,10 @@ if __name__ == '__main__':
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     lists = img2lists(image_processed)
-    print(':: Start painting after 5 seconds')
+    print(':: Start drawing after 5 seconds')
     sleep(5)
-    draw(lists, scale=2)  # set scale of drawing
+    start_time = time()
+    # set scale of drawing, switch draw_position_point, and interlaced
+    draw(lists, scale=2, draw_position_point=True, interlaced=True)
+    end_time = time()
+    print(f"Time cost: {end_time - start_time:.3f} s")
